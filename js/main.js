c5b74748650e5ca15a7cc3b4f8276f96ad24cd16
Elena Rentero <elenaclementerentero@gmail.com>
2022-10-31 10:27:06 +0100

    update values pc selection

--- js/main.js
+++ js/main.js
@@ -4,11 +4,6 @@ const select = document.querySelector('.js-select');
 const btn = document.querySelector('.js-btn');
 const paragraph = document.querySelector('js-paragraph');
 let random = 0;
-let surenos = 2;
-let orcos = 2;
-let goblins = 2; 
-let huargos = 3;
-let trolls = 5;
 
 // Funcion que recoge el valor de la opcion elegida por el usuario
 function userSelection(){
@@ -21,21 +16,40 @@ function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
     }
 
-// Funcion que compara ambos valores
-function compareOptions(selectValue, random){
-    if (selectValue > random){
-        console.log ("ganan los buenos")
+// Funcion para cambiar el random number por el valor asignado
+function pcSelection(random){
+    if (random === 1 || random === 2 || random === 3){
+        return 2
+    } else if (random === 4){
+        return 3
     } else {
-        console.log("ganan los malos")
+        return 5
     }
 }
+
+// Funcion que compara ambos valores
+function compareOptions(userValue, pcValue){
+    if (userValue > pcValue){
+        console.log (`¡Ha ganado el Ejército del Bien! Enhorabuena.`)
+    } else if (userValue === pcValue) {
+        console.log (`Empate.`)
+    } else {
+        console.log (`¡Ha ganado el Ejército del Mal! Vuelve a intentarlo.`)
+    }
+}
+
+/* Funcion que pinta en el HTML
+function paintHTML (result){
+    paragraph.innerHTML = result;
+}*/
 
 // Funcion manejadora
 function handleClick(event){
     event.preventDefault();
-    random = getRandomNumber(6);
-    const selectValue = userSelection();
-    compareOptions(selectValue, random);
+    random = getRandomNumber(5);
+    const userValue = userSelection();
+    const pcValue = pcSelection(random);
+    compareOptions(userValue, pcValue);
 }
 
 // Evento que escucha el click en el boton
